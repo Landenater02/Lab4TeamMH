0980b110f6e28a1d09cd82ac8516d80a427d47fc
Landenater02 <hawlel75@uwosh.edu>
2025-10-05 18:54:16 -0500

    Added the calculator class

--- Program.cs
+++ Program.cs
@@ -1,9 +1,25 @@
-﻿
-namespace Lab4TeamMH
+﻿class Program {
+static void Main(String [] args){
+Console.WriteLine(Calculator.Add(3,4));
+Console.WriteLine(Calculator.Subtract(3,4));
+}
+}
+public static class Calculator
 {
-
-    public class Calculator
-    {
-
-    }
+public static int Add(int a, int b)
+{
+return 0;
+}
+public static int Subtract(int a, int b)
+{
+return 0;
+}
+public static int Multiply(int a, int b)
+{
+return 0;
+}
+public static int Divide(int a, int b)
+{
+return 0;
+}
 }
